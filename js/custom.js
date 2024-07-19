d4fd72dbb517c4625ebe18ee4503d4056848e9c8
Phạm Tuấn Anh <tuananhtybg.99@gmail.com>
2024-07-19 08:16:16 +0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+/*====================================================
+             Gallery
+====================================================*/
+$(function () {
+    $("#Gallery").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
